d05f4f32788d2675d57f3426b043b1477a321818
Yuming Wang <wgyumg@gmail.com>
2016-10-13 15:16:01 +0800

    Update cookie

--- YhdCreateWorklog.sh
+++ YhdCreateWorklog.sh
@@ -2,7 +2,7 @@
 
 today=`date '+%Y-%m-%d'`
 
-spark-submit --class com.yhd.sql.YhdCreateWorklog /tmp/spark-examples-0.1.0-SNAPSHOT-jar-with-dependencies.jar "wangyuming" "Hm_lvt_ce7370daf17a054325d348babea3ccd6=1474335301; atlassian.xsrf.token=BJOO-3RWZ-LXVM-C1H0|2ceaae3ca86ab58c5ac42e5777704d0a1b8fd8e0|lin; JSESSIONID=4CEB987C952B110CC301E2C74D941D2A; Hm_lpvt_ce7370daf17a054325d348babea3ccd6=1476266105" "${today}" 8.0
+spark-submit --class com.yhd.sql.YhdCreateWorklog /tmp/spark-examples-0.1.0-SNAPSHOT-jar-with-dependencies.jar "wangyuming" "Hm_lvt_ce7370daf17a054325d348babea3ccd6=1474852250; JSESSIONID=D71CFF9F1A4C63B91839DE01E58FE399; atlassian.xsrf.token=BJOO-3RWZ-LXVM-C1H0|565fe26f3fc29310834a4b4957c589d020c73d71|lin; Hm_lpvt_ce7370daf17a054325d348babea3ccd6=1476342794; seraph.rememberme.cookie=98954"%"3A3d13d25dc43ab6f9b0510764c4a49e4b4daa25be" "${today}" 8.0
 
 # crontab commond
 # 30 18 * * * source /etc/profile && sh /root/opensource/spark-examples/YhdCreateWorklog.sh 2>&1
